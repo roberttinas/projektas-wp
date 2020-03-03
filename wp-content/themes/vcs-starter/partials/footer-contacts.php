c0b62642a035fa653a485e15149e2393081cc355
roberttinas <roberttinas@gmail.com>
2020-03-03 17:55:40 +0200

    Create footer-contacts.php

---
+++ wp-content/themes/vcs-starter/partials/footer-contacts.php
@@ -0,0 +1,112 @@
+<!-- FOOTER CONTACTS ================================================== -->
+<section class="bg-img">
+    <section class="footer-contacts">
+        <div class="container">
+            <div class="logo-footer">
+                <a href="#">
+                    <img src="assets/images/img_logo.jpg" alt="logo">
+                </a>
+            </div>
+            <div class="menu-items">
+                <h3>
+                    Menu
+                </h3>
+                <ul>
+                    <li>
+                        <a href="#">
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            About us
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Features
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Portfolio
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="menu-items social">
+                <h3></h3>
+                <ul>
+                    <li>
+                        <a href="#">
+                            Service
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Blog
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Blog pages
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://facebook.com" target="_blank">
+                            <i class="fab fa-facebook-f fa-fw"></i>
+                        </a>
+                        <a href="http://twitter.com" target="_blank">
+                            <i class="fab fa-twitter fa-fw"></i>
+                        </a>
+                        <a href="http://google.com" target="_blank">
+                            <i class="fab fa-google-plus-g fa-fw"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="contact-items">
+                <h3>
+                    Contact
+                </h3>
+                <ul>
+                    <li>
+                        <a href="http://maps.google.com" target="_blank">
+                            <span>Address : </span>Geethanjali street second floor: 88569
+                        </a>
+                    </li>
+                    <li>
+                        <a href="tel:8877-8564-222">
+                            <span>Phone : </span>8877-8564-222
+                        </a>
+                    </li>
+                    <li>
+                        <a href="mailto:info@business">
+                            <span>Email : </span>info@business.com
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="newsletter">
+                <h3>
+                    Newsletter</h3>
+                <p>
+                    Sign up your newsletter</p>
+                <div class="input-form">
+                    <input type="email" name="email" id="email_id" placeholder="Email address">
+                    <button type="submit" class="btn-submit">
+                        Go
+                    </button>
+                </div>
+            </div>
+        </div>
+    </section>
+<!-- COPYRIGHT ================================================== -->
+    <section class="copyright">
+        <div class="container">
+            <h5>
+                2015 business ltd. allright reserved
+            </h5>
+        </div>
+    </section>
+</section>
