58c0945b6871e9e3379de7ffeb6356eddf936754
roberttinas <roberttinas@gmail.com>
2020-03-03 17:52:26 +0200

    Add features section to hompage.php

--- wp-content/themes/vcs-starter/templates/homepage.php
+++ wp-content/themes/vcs-starter/templates/homepage.php
@@ -9,6 +9,7 @@ get_template_part('partials/nav-menu');
 get_template_part('partials/hero');
 get_template_part('partials/introduction');
 get_template_part('partials/clients');
+get_template_part('partials/features');
 ?>
 
 <!-- Start Point -->
