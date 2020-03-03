96a85ed5a57a8b84d7f05c03f05d05ff0af0c63b
roberttinas <roberttinas@gmail.com>
2020-03-03 17:58:16 +0200

    Create team.php

---
+++ wp-content/themes/vcs-starter/partials/team.php
@@ -0,0 +1,135 @@
+<!-- TEAM MEMBERS ================================================== -->
+<section class="team">
+    <div class="container">
+        <div class="team-header">
+            <h2>
+                Team
+            </h2>
+            <h3>
+                Meet our team
+            </h3>
+        </div>
+        <div class="team-info">
+            <div class="team-members">
+                <div class="team-photo">
+                    <img src="assets/images/team/img_team_1.jpg" alt="ramgopalavarna">
+                </div>
+                <div class="team-contacts">
+                    <div class="specialist">
+                        Mr. Ramgopalavarna
+                    </div>
+                    <div class="duty">
+                        Data Specialist
+                    </div>
+                    <ul class="btn-social">
+                        <li>
+                            <a href="http://facebook.com" target="_blank">
+                                <i class="fab fa-facebook-f fa-fw"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://twitter.com" target="_blank">
+                                <i class="fab fa-twitter fa-fw"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://google.com" target="_blank">
+                                <i class="fab fa-google-plus-g fa-fw"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="team-members">
+                <div class="team-photo">
+                    <img src="assets/images/team/img_team_2.jpg" alt="menon">
+                </div>
+                <div class="team-contacts">
+                    <div class="specialist">
+                        Madhavikutti Menon
+                    </div>
+                    <div class="duty">
+                        Client Support
+                    </div>
+                    <ul class="btn-social">
+                        <li>
+                            <a href="http://facebook.com" target="_blank">
+                                <i class="fab fa-facebook-f fa-fw"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://twitter.com" target="_blank">
+                                <i class="fab fa-twitter fa-fw"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://google.com" target="_blank">
+                                <i class="fab fa-google-plus-g fa-fw"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="team-members">
+                <div class="team-photo">
+                    <img src="assets/images/team/img_team_3.jpg" alt="venkittaraman">
+                </div>
+                <div class="team-contacts">
+                    <div class="specialist">
+                        Dr. Venkittaraman
+                    </div>
+                    <div class="duty">
+                        Client Management
+                    </div>
+                    <ul class="btn-social">
+                        <li>
+                            <a href="http://facebook.com" target="_blank">
+                                <i class="fab fa-facebook-f fa-fw"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://twitter.com" target="_blank">
+                                <i class="fab fa-twitter fa-fw"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://google.com" target="_blank">
+                                <i class="fab fa-google-plus-g fa-fw"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="team-members">
+                <div class="team-photo">
+                    <img src="assets/images/team/img_team_4.jpg" alt="danial">
+                </div>
+                <div class="team-contacts">
+                    <div class="specialist">
+                        George Danial
+                    </div>
+                    <div class="duty">
+                        Communication
+                    </div>
+                    <ul class="btn-social">
+                        <li>
+                            <a href="http://facebook.com" target="_blank">
+                                <i class="fab fa-facebook-f fa-fw"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://twitter.com" target="_blank">
+                                <i class="fab fa-twitter fa-fw"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://google.com" target="_blank">
+                                <i class="fab fa-google-plus-g fa-fw"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
