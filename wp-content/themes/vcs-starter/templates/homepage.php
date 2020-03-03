d0a38a0417880f9a86712ce78b31fec6cd88934b
roberttinas <roberttinas@gmail.com>
2020-03-03 17:53:34 +0200

    Add footer-contacts section to hompage.php

--- wp-content/themes/vcs-starter/templates/homepage.php
+++ wp-content/themes/vcs-starter/templates/homepage.php
@@ -13,6 +13,7 @@ get_template_part('partials/features');
 get_template_part('partials/pricing');
 get_template_part('partials/team');
 get_template_part('partials/sign-up');
+get_template_part('partials/footer-contacts');
 ?>
 
 <!-- Start Point -->
