3c62e94bc8562c995019483af44fed1b405f0bfd
roberttinas <roberttinas@gmail.com>
2020-03-03 17:57:46 +0200

    Create pricing.php

---
+++ wp-content/themes/vcs-starter/partials/pricing.php
@@ -0,0 +1,127 @@
+<!-- PRICING ================================================== -->
+<section class="pricing">
+    <div class="container">
+        <div class="pricing-header">
+            <h2>
+                Pricing
+            </h2>
+            <h3>
+                Some of our features
+            </h3>
+        </div>
+        <div class="pricing-info">
+            <div class="pricing-options">
+                <div class="options">
+                    <div class="prices">
+                        <h3>
+                            Personal
+                        </h3>
+                        <h4>
+                            From <span>$100</span> per month
+                        </h4>
+                    </div>
+                    <ul class="benefits">
+                        <li>
+                            Business support
+                        </li>
+                        <li>
+                            Business analysing
+                        </li>
+                        <li>
+                            24 hour support
+                        </li>
+                    </ul>
+                </div>
+                <div class="btn">
+                    <a href="#">
+                        Start now
+                    </a>
+                </div>
+            </div>
+            <div class="pricing-options recommend">
+                <div class="prices">
+                    <h3>
+                        Business
+                    </h3>
+                    <h4>
+                        From <span>$200</span> per month
+                    </h4>
+                </div>
+                <ul class="benefits">
+                    <li>
+                        Business support
+                    </li>
+                    <li>
+                        Business analysing
+                    </li>
+                    <li>
+                        24 hour support
+                    </li>
+                    <li>
+                        Client management
+                    </li>
+                </ul>
+                <div class="btn">
+                    <a href="#">
+                        Start now
+                    </a>
+                </div>
+            </div>
+            <div class="pricing-options">
+                <div class="prices">
+                    <h3>
+                        Gold
+                    </h3>
+                    <h4>
+                        From <span>$250</span> per month
+                    </h4>
+                </div>
+                <ul class="benefits">
+                    <li>
+                        24 hour support
+                    </li>
+                    <li>
+                        Client management
+                    </li>
+                    <li>
+                        Keeping business data
+                    </li>
+                </ul>
+                <div class="btn">
+                    <a href="#">
+                        Start now
+                    </a>
+                </div>
+            </div>
+            <div class="pricing-options">
+                <div class="prices">
+                    <h3>
+                        Ultimate
+                    </h3>
+                    <h4>
+                        From <span>$300</span> per month
+                    </h4>
+                </div>
+                <ul class="benefits">
+                    <li>
+                        24 hour support
+                    </li>
+                    <li>
+                        Client management
+                    </li>
+                    <li>
+                        Keeping business data
+                    </li>
+                    <li>
+                        Better communication
+                    </li>
+                </ul>
+                <div class="btn">
+                    <a href="#">
+                        Start now
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
