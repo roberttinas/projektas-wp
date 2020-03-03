6368cb6c80fbec40e8cd2ef2c24f510583d2b64f
roberttinas <roberttinas@gmail.com>
2020-03-03 17:58:03 +0200

    Create sign-up.php

---
+++ wp-content/themes/vcs-starter/partials/sign-up.php
@@ -0,0 +1,19 @@
+<!-- SIGN UP FORM ================================================== -->
+<section class="sign-up">
+    <div class="container">
+        <h1>
+            One month <span>free</span> service
+        </h1>
+        <h3>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+        </h3>
+        <div class="btn-contacts">
+            <a href="#">
+                Sign up
+            </a>
+            <a href="#">
+                Contact us
+            </a>
+        </div>
+    </div>
+</section>
