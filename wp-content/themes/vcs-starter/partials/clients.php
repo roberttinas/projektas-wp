a7c869d97b773b10f14fdb3d43d2662fab847d16
roberttinas <roberttinas@gmail.com>
2020-03-03 17:54:45 +0200

    Create clients.php

---
+++ wp-content/themes/vcs-starter/partials/clients.php
@@ -0,0 +1,60 @@
+<!-- CLIENTS ================================================== -->
+<section class="clients">
+    <div class="container">
+        <div class="clients-header">
+            <h2>
+                Clients
+            </h2>
+            <h3>
+                Customers says about us
+            </h3>
+        </div>
+        <div class="clients-info flex-container">
+            <div class="qmark">
+                <i class="fas fa-quote-left"></i>
+            </div>
+            <div class="clients-header-info">
+                <p class="clients-info-description">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
+                </p>
+                <p class="author">
+                    By
+                </p>
+                <p class="author_name">
+                    George Danial
+                </p>
+            </div>
+            <div class="clients-images flex-container">
+                <div class="slider-container">
+                    <div class="photo">
+                        <img src="assets/images/clients/img_clients_1.jpg" alt="clients images">
+                    </div>
+                    <div class="photo">
+                        <img src="assets/images/clients/img_clients_2.jpg" alt="clients images">
+                    </div>
+                    <div class="photo">
+                        <img src="assets/images/clients/img_clients_3.jpg" alt="clients images">
+                    </div>
+                    <div class="photo">
+                        <img src="assets/images/clients/img_clients_4.jpg" alt="clients images">
+                    </div>
+                    <div class="photo">
+                        <img src="assets/images/clients/img_clients_5.jpg" alt="clients images">
+                    </div>
+                    <div class="photo">
+                        <img src="assets/images/clients/img_clients_6.jpg" alt="clients images">
+                    </div>
+                    <div class="photo">
+                        <img src="assets/images/clients/img_clients_7.jpg" alt="clients images">
+                    </div>
+                    <div class="photo">
+                        <img src="assets/images/clients/img_clients_8.jpg" alt="clients images">
+                    </div>
+                    <div class="photo">
+                        <img src="assets/images/clients/img_clients_9.jpg" alt="clients images">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
