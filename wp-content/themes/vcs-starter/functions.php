b9690000b44d4c3315abe5d4b42dc5e4096959ae
roberttinas <roberttinas@gmail.com>
2020-03-04 09:23:59 +0200

    Delete unnecessary content from functions.php

--- wp-content/themes/vcs-starter/functions.php
+++ wp-content/themes/vcs-starter/functions.php
@@ -17,15 +17,7 @@ if( !defined('THEME_FOLDER') ) {
 function theme_scripts(){
 
     if ( !is_admin() ) {
-
-    	//wp_register_script(handle, path, dependency, version, load_in_footer);
-
-        //wp_deregister_script('jquery');
-		//wp_register_script('jquery', THEME_FOLDER . '/assets/js/jquery-2.1.1.js', false, '2.1.1', true);
-
     	//Registration
-        wp_register_script('js_main', THEME_FOLDER . '/assets/js/main.js', array('jquery'), '1.0', true);
-
 		wp_register_script('fa', 'https://kit.fontawesome.com/8f49103de5.js', false, false, false);
 		wp_deregister_script('jquery');
 		wp_register_script('jquery', THEME_FOLDER . '/assets/scripts/jquery.js', array('fa'), false, true);
@@ -33,7 +25,6 @@ function theme_scripts(){
 		wp_register_script('slider', THEME_FOLDER . '/assets/scripts/tiny-slider.js', array('fancybox'), false, true);
 		wp_register_script('main-js', THEME_FOLDER . '/assets/scripts/projektas.js', array('slider'), false, true);
         //Loading
-        wp_enqueue_script('js_main');
 		wp_enqueue_script('fa');
 		wp_enqueue_script('jquery');
         wp_enqueue_script('fancybox');
@@ -47,11 +38,7 @@ add_action('wp_enqueue_scripts', 'theme_scripts');
 function theme_stylesheets(){
 
 	if( defined('THEME_FOLDER') ) {
-		//wp_register_style(handle, path, dependency, version, devices);	
-
 		//Registration
-		wp_register_style('main-css', THEME_FOLDER . '/assets/css/main.css', array(), false, 'all');
-
 		wp_register_style('google-fonts', 'https://fonts.googleapis.com/css?family=Karla|Lora:400i|Open+Sans|PT+Sans|Raleway:400,500,700,800|Roboto+Slab&display=swap&subset=latin-ext', array(), false, 'all');
 		wp_register_style('main-css', THEME_FOLDER . '/assets/css/style.css', array('google-fonts'), false, 'all');
 		wp_register_style('slider', THEME_FOLDER . '/assets/css/tiny-slider.css', array('main-css'), false, 'all');
@@ -68,7 +55,6 @@ add_action('wp_enqueue_scripts', 'theme_stylesheets');
 function register_theme_menus() {
    
 	register_nav_menus(array( 
-        'primary-navigation' => __( 'Primary Navigation' ) 
 		'secondary-navigation' => __( 'Secondary Navigation' ),
     ));
 }
@@ -98,7 +84,6 @@ if( isset($sidebars) && !empty($sidebars) ) {
 		));
 	}
 }
-
 
 class custom_navwalker extends Walker_Nav_Menu {
 
