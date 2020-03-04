ea8f32dec62a89e6e84e578cd06dedc7175292f6
roberttinas <roberttinas@gmail.com>
2020-03-04 09:30:12 +0200

    Update hero.php
    Delete static content

--- wp-content/themes/vcs-starter/partials/hero.php
+++ wp-content/themes/vcs-starter/partials/hero.php
@@ -1,12 +1,9 @@
 <!-- HERO ================================================== -->
-<section class="hero" style="background-image: url(assets/images/welcome/img_welcome_2.jpg);">
     <div class="overlay flex-container">
         <div class="container">
             <h1>
-                Boost Up Your Local Business
             </h1>
             <h4>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
             </h4>
         </div>
     </div>
