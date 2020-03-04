b8227fad3bf8e14606e3331228b05a8c44474fda
roberttinas <roberttinas@gmail.com>
2020-03-04 09:40:38 +0200

    Update sign-up.php
    Delete static content

--- wp-content/themes/vcs-starter/partials/sign-up.php
+++ wp-content/themes/vcs-starter/partials/sign-up.php
@@ -2,18 +2,10 @@
 <section class="sign-up">
     <div class="container">
         <h1>
-            One month <span>free</span> service
         </h1>
         <h3>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
         </h3>
         <div class="btn-contacts">
-            <a href="#">
-                Sign up
-            </a>
-            <a href="#">
-                Contact us
-            </a>
         </div>
     </div>
 </section>
