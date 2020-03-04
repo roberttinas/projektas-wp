accb98ce0378c2a2bf5d761b2c39fd76e38d854c
roberttinas <roberttinas@gmail.com>
2020-03-04 09:24:14 +0200

    Update functions.php

--- wp-content/themes/vcs-starter/functions.php
+++ wp-content/themes/vcs-starter/functions.php
@@ -218,6 +218,7 @@ class custom_navwalker extends Walker_Nav_Menu {
 		 */
 		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
 	}
+}
 function dump($data){
 	echo "<pre>";
 	print_r($data);
