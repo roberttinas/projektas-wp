b91e5a7186137a596b2786b4af7afb4f14970be4
roberttinas <roberttinas@gmail.com>
2020-03-04 09:28:47 +0200

    Update features.php
    Add section heading, subheading, repeater function, link

--- wp-content/themes/vcs-starter/partials/features.php
+++ wp-content/themes/vcs-starter/partials/features.php
@@ -3,13 +3,43 @@
     <div class="container">
         <div class="features-header">
             <h2>
+                <?php the_field('fb_section_heading'); ?>
             </h2>
             <h3>
+                <?php the_field('fb_section_subheading'); ?>
             </h3>
         </div>
         <div class="features-info">
+            <?php
+            if(have_rows('fb_features_repeater')):
+                while(have_rows('fb_features_repeater')):
+                    the_row();
+                    ?>
+                    <div class="features-info-description">
+                        <div class="features-icon">
+                            <?php the_sub_field('icon'); ?>
+                        </div>
+                        <div class="text">
+                            <h3>
+                                <?php the_sub_field('heading'); ?>
+                            </h3>
+                            <p>
+                                <?php the_sub_field('description'); ?>
+                            </p>
+                        </div>
+                    </div>
+                    <?php
+                endwhile;
+            endif;
+            ?>
         </div>
         <div class="object">
+            <?php
+            $link = get_field('fb_link');
+            $target = $link['target'] ? ' target="_blank" ' : "";
+            ?>
+            <a href="<?php echo $link['url']; ?>" <?php echo $target; ?>>
+                <?php echo $link['title']; ?>
             </a>
         </div>
     </div>
