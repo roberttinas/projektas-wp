a34ac097819d96a6b2c6ef99736babccb34b5aa6
roberttinas <roberttinas@gmail.com>
2020-03-04 09:32:34 +0200

    Update introduction.php
    Delete static content

--- wp-content/themes/vcs-starter/partials/introduction.php
+++ wp-content/themes/vcs-starter/partials/introduction.php
@@ -4,60 +4,15 @@
         <div class="introduction-content">
             <div class="introduction-header">
                 <h2>
-                    Make Your Startup
                 </h2>
                 <h4>
-                    Dream a reality
                 </h4>
             </div>
             <p class="startup">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap	into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset.
             </p>
-            <img src="assets/images/img_startup.jpg" alt="Plant growning" class="photos">
         </div>
         <div class="introduction-benefits">
-            <div class="introduction-benefits-info flex-container">
-                <div class="benefits-icons">
-                    <i class="far fa-chart-bar fa-fw"></i>
-                </div>
-                <div class="benefits-features">
-                    <h3 class="introduction-info-header">
-                        Boost your business
-                    </h3>
-                    <p class="benefits">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.
-                    </p>
-                </div>
-            </div>
-            <div class="introduction-benefits-info flex-container">
-                <div class="benefits-icons">
-                    <i class="fas fa-clipboard-list fa-fw"></i>
-                </div>
-                <div class="benefits-features">
-                    <h3 class="introduction-info-header">
-                        Filtering business data
-                    </h3>
-                    <p class="benefits">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.
-                    </p>
-                </div>
-            </div>
-            <div class="introduction-benefits-info flex-container">
-                <div class="benefits-icons">
-                    <i class="fab fa-black-tie fa-fw"></i>
-                </div>
-                <div class="benefits-features">
-                    <h3 class="introduction-info-header">
-                        Business solutions
-                    </h3>
-                    <p class="benefits">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.
-                    </p>
-                </div>
-            </div>
             <div class="benefits-features-btn">
-                <a href="#" class="btn-style">
-                    More services
                 </a>
             </div>
         </div>
