122334f2aca868191f55e9a089ccddf3960ea018
roberttinas <roberttinas@gmail.com>
2020-03-04 09:30:59 +0200

    Update hero.php
    Add bg image, section heading and subheading

--- wp-content/themes/vcs-starter/partials/hero.php
+++ wp-content/themes/vcs-starter/partials/hero.php
@@ -1,9 +1,19 @@
 <!-- HERO ================================================== -->
+<?php
+$image = get_field('hb_background_image');
+// dump($image);
+?>
+<section class="hero" style="background-image: url(<?php echo $image['sizes']['1536x1536']; ?>);">
     <div class="overlay flex-container">
         <div class="container">
             <h1>
+                <?php
+                $heading = get_field('hb_section_heading');
+                echo nl2br($heading);
+                ?>
             </h1>
             <h4>
+                <?php the_field('hb_section_description'); ?>
             </h4>
         </div>
     </div>
