2bff5a2de4e663a657db5f0ccda21ca0a5e172f1
roberttinas <roberttinas@gmail.com>
2020-03-04 09:27:52 +0200

    Delete static content from features.php

--- wp-content/themes/vcs-starter/partials/features.php
+++ wp-content/themes/vcs-starter/partials/features.php
@@ -3,95 +3,13 @@
     <div class="container">
         <div class="features-header">
             <h2>
-                Features
             </h2>
             <h3>
-                Some of our features
             </h3>
         </div>
         <div class="features-info">
-            <div class="features-info-description">
-                <div class="features-icon">
-                    <i class="fas fa-server fa-fw"></i>
-                </div>
-                <div class="text">
-                    <h3>
-                        Data Entry
-                    </h3>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                    </p>
-                </div>
-            </div>
-            <div class="features-info-description">
-                <div class="features-icon">
-                    <i class="far fa-handshake fa-fw"></i>
-                </div>
-                <div class="text">
-                    <h3>
-                        Client management
-                    </h3>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                    </p>
-                </div>
-            </div>				
-            <div class="features-info-description">
-                <div class="features-icon">
-                    <i class="fas fa-network-wired fa-fw"></i>
-                </div>
-                <div class="text">
-                    <h3>
-                        Better Communication
-                    </h3>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                    </p>
-                </div>
-            </div>
-            <div class="features-info-description">
-                <div class="features-icon">
-                    <i class="fas fa-chart-pie fa-fw"></i>
-                </div>
-                <div class="text">
-                    <h3>
-                        Well analyzing team
-                    </h3>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                    </p>
-                </div>
-            </div>
-            <div class="features-info-description">
-                <div class="features-icon">
-                    <i class="fas fa-headset fa-fw"></i>
-                </div>
-                <div class="text">
-                    <h3>
-                        24 hour support
-                    </h3>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                    </p>
-                </div>
-            </div>
-            <div class="features-info-description">
-                <div class="features-icon">
-                    <i class="fas fa-briefcase fa-fw"></i>
-                </div>
-                <div class="text">
-                    <h3>
-                        Keeping business data
-                    </h3>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                    </p>
-                </div>
-            </div>
         </div>
         <div class="object">
-            <a href="#">
-                More features
             </a>
         </div>
     </div>
